e1b36bd764283c572296572ae7fd621f61ac85da
Richard Pitts <richardepitts@gmail.com>
2019-07-17 17:40:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,11 @@
 def introduction(name) 
-  "Hi, my name is #{name}."
+  return "Hi, my name is #{name}."
 end 
 
 def introduction_with_language(name, language) 
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+  return "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 def introduction_with_language_optional(name, language = "Ruby") 
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+  return "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
